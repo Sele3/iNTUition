5a6cbb57215323e56cc9344bbfed2dfd886af2be
geraldd-d <geraldong12345@gmail.com>
2023-02-26 04:13:56 +0800

    d

--- frontend/src/components/ToggleButtons.jsx
+++ frontend/src/components/ToggleButtons.jsx
@@ -1,30 +1,31 @@
 
 export function ToggleButtons(props){
     return (
-        <div className="flex space-x-2">
-          <button style = {{height : "5%"}}
-            className={`bg-blue-500 text-white font-bold py-2 px-4 rounded ${
-              props.mode === "summary" ? "bg-blue-700" : ""
+        <div className="">
+          <button style = {{position: "absolute", height : "7%", left : "40%", top: "18%"}}
+            className={` text-white font-thin py-2 px-4 rounded ${
+              props.mode === "summary" ? "bg-gray-700" : ""
             }`}
             onClick={props.onSum}
           >
             Summary
           </button>
-          <button style = {{height : "5%"}}
-            className={`bg-gray-400 text-black font-bold py-2 px-4 rounded ${
+          
+          <button style = {{position: "absolute", height : "7%", left : "57%", top: "18%"}}
+            className={` text-white font-thin py-2 px-4 rounded ${
+              props.mode === "info" ? "bg-gray-700" : ""
+            }`}
+            onClick={props.onInfo}
+          >
+            Image
+          </button>
+      <button style = {{position: "absolute", height : "7%", left : "48%", top: "18%"}}
+            className={` text-white font-thin py-2 px-4 rounded ${
               props.mode === "ppt" ? "bg-gray-700" : ""
             }`}
             onClick={props.onPpt}
           >
             PowerPoint
-          </button>
-          <button style = {{height : "5%"}}
-            className={`bg-yellow-500 text-black font-bold py-2 px-4 rounded ${
-              props.mode === "info" ? "bg-yellow-700" : ""
-            }`}
-            onClick={props.onInfo}
-          >
-            Infographic
           </button>
         </div>
       );
