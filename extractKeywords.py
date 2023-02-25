ae69ee0512c4fd09359ac73d8063e31941f1891f
unknown <weisiongp@gmail.com>
2023-02-26 00:20:09 +0800

    Added infographic generator.

--- extractKeywords.py
+++ extractKeywords.py
@@ -2,7 +2,9 @@ import pke
 import openai
 from nltk.tokenize import sent_tokenize
 import replicate
+import os
 from main import extract_pdf_file
+from urllib.request import urlretrieve
 MAX_LENGTH = 1000
 
 ABBR = {"cvd": "Cardiovascular disease", "t2dm": "Type 2 diabetes", "incident cvd": "Incident Cardiovascular disease"}
@@ -66,15 +68,57 @@ def tf_model(rt):
     return return_top_keywords(keyphrases)
 
 
+def infographic(key, filepath):
+    #\frontend\public\media
+    #
+    os.environ["REPLICATE_API_TOKEN"] = ""
+    model = replicate.models.get("stability-ai/stable-diffusion")
+    version = model.versions.get("db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf")
+    prompt = ",".join(['infographic', 'medical']+key)
+
+    inputs = {
+    # Input prompt
+    'prompt': prompt,
+
+    # pixel dimensions of output image
+    'image_dimensions': "768x768",
+
+    # Specify things to not see in the output
+    # 'negative_prompt': ...,
+
+    # Number of images to output.
+    # Range: 1 to 4
+    'num_outputs': 1,
+
+    # Number of denoising steps
+    # Range: 1 to 500
+    'num_inference_steps': 50,
+
+    # Scale for classifier-free guidance
+    # Range: 1 to 20
+    'guidance_scale': 7.5,
+
+    # Choose a scheduler.
+    'scheduler': "DPMSolverMultistep",
+
+    # Random seed. Leave blank to randomize the seed
+    # 'seed': ...,
+}
+
+    print(prompt)
+    link = version.predict(**inputs)
+    urlretrieve(link[0], os.path.join(os.path.dirname(__file__), os.getcwd() + filepath))
+
 def main():
 
-    openai.api_key = "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"
-
+    #extract file from filepath
     rt = extract_pdf_file("s12916-023-02774-1.pdf")
 
     #Extract Keywords from chunks
     keywords = tf_model(rt)
-    print(keywords)
+
+
+    infographic(keywords, "\\frontend\public\media\infographic.png")
 
 
 
