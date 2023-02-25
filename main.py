3773962cedf7f83cd167acb4fab725bbbb5fc300
Sele3 <STAN266@e.ntu.edu.sg>
2023-02-25 21:33:13 +0800

    wip: bert summarizer

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from PyPDF2 import PdfReader
-from summarizer import *
+#from summarizer import *
 import re
+from bert import bert_summarizer
+
 
 app = FastAPI()
 
@@ -20,7 +22,14 @@ app.add_middleware(
 async def create_upload_file(file: UploadFile):
     extracted_text = extract_pdf_file(file.file)
 
+    return {"filename": bert_summarizer(extracted_text)}
+
+    extracted_text = extract_pdf_file(file.file)
+    
     summarized_text = text_summarizer(extracted_text)
+
+    with open('sample.txt', 'w', encoding='utf-8') as f:
+        f.write(extracted_text)
 
     return {"filename": summarized_text}
 
@@ -29,20 +38,24 @@ def extract_pdf_file(file) -> str:
     pdf_reader : PdfReader = PdfReader(file)
     result = ""
 
-    with open('sample.txt', 'w', encoding='utf-8') as f:
-        for page_num in range(1, len(pdf_reader.pages)-1):
-            page = pdf_reader.pages[page_num]
-            result += page.extract_text()
-            
-        f.write(result)
+    for page_num in range(1, len(pdf_reader.pages)-1):
+        page = pdf_reader.pages[page_num]
+        result += page.extract_text()
 
     return clean_text(result)
     
 
 
 def clean_text(s: str) -> str:
-    # # Remove 'References' at end of paper
-    # s = ''.join(s.split('References')[:-1])
+    # Remove newline
+    s = s.replace('\n', ' ')
+    
+    # Remove 'References' at end of paper
+    temp = s.split('References')
+    s = ''.join(temp[:-1]) if len(temp) > 1 else temp[0]
+
+    # Remove Page Numbers
+    s = re.sub(r'Page \d+ of \d+ ', '', s)
 
     # Remove text within square or round brackets
     s = re.sub(r'[\[\(].*?[\]\)]', '', s)
@@ -56,7 +69,7 @@ def clean_text(s: str) -> str:
     # Remove extra white spaces
     s = re.sub(r'\s\s+', ' ', s)
 
-    # Remove newline
-    s = s.replace(r'\n', ' ')
-
+    # Remove white spaces in front of punctuations
+    s = re.sub(r'\s(?=[\.,;-])', '', s)
+    
     return s
