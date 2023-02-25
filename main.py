d8351b86658627305679cd735dbda0724fd23d07
Sele3 <STAN266@e.ntu.edu.sg>
2023-02-25 14:52:41 +0800

    feat: pdf to text

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
-from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+from PyPDF2 import PdfReader
+import re
 
 app = FastAPI()
 
 
-@app.get("/api")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/api/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    extract_pdf_file(file.file)
+    
+    return {"filename": file.filename}
 
 
-@app.get("/api/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def extract_pdf_file(file) -> str:
+    pdf_reader : PdfReader = PdfReader(file)
+    
+    text = ""
+    with open('sample.txt', 'w', encoding='utf-8') as f:
+        for page_num in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num]
+            text += page.extract_text()
+            print('page', page_num, len(text))
+            
+        text = clean_text(text)
+        f.write(text)
 
+    print('done')
+    return text
+
+
+def clean_text(s: str) -> str:
+    # Remove newline
+    s = s.replace('\n', ' ')
+    # Remove 'References' at end of paper
+    s = ''.join(s.split('References')[:-1])
+    # Remove text within square brackets
+    s = re.sub(r'\[.*?\]', '', s)
+    # Remove extra white spaces
+    s = re.sub(r'\s\s+', ' ', s)
+
+    return s
