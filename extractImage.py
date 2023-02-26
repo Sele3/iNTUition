61d689e0f2f4d20d1f0a743f182d473c52abe67b
geraldd-d <geraldong12345@gmail.com>
2023-02-26 10:06:41 +0800

    Create extractImage.py

---
+++ extractImage.py
@@ -0,0 +1,34 @@
+import fitz
+import os
+from PIL import Image
+
+file_path = 'file5.pdf'
+pdf_file = fitz.open(file_path)
+
+#Calculate number of pages in PDF file
+page_nums = len(pdf_file)
+
+#Create empty list to store images information
+images_list = []
+images_path = './assets'
+
+#Extract all images information from each page
+for page_num in range(page_nums):
+    page_content = pdf_file[page_num]
+    images_list.extend(page_content.get_images())
+
+for i, image in enumerate(images_list, start=1):
+    #Extract the image object number
+    xref = image[0]
+    #Extract image
+    base_image = pdf_file.extract_image(xref)
+    #Store image bytes
+    image_bytes = base_image['image']
+    #Store image extension
+    image_ext = base_image['ext']
+    #Generate image file name
+    image_name = str(i) + '.' + image_ext
+    #Save image
+    with open(os.path.join(images_path, image_name) , 'wb') as image_file:
+        image_file.write(image_bytes)
+        image_file.close()
